e7e6c583b676325d64e10cf75257c000e35cd344
Ilya Kitaev <mbg033@gmail.com>
2016-10-07 16:20:52 +0300

    Adding sort-filter-proxy model

---
+++ src/model/TransactionHistorySortFilterModel.cpp
@@ -0,0 +1,6 @@
+#include "TransactionHistorySortFiltrerModel.h"
+
+TransactionHistorySortFiltrerModel::TransactionHistorySortFiltrerModel()
+{
+
+}
---
+++ src/model/TransactionHistorySortFilterModel.h
@@ -0,0 +1,12 @@
+#ifndef TRANSACTIONHISTORYSORTFILTRERMODEL_H
+#define TRANSACTIONHISTORYSORTFILTRERMODEL_H
+
+#include <QObject>
+
+class TransactionHistorySortFiltrerModel : public QSortFilterProxyModel
+{
+public:
+    TransactionHistorySortFiltrerModel();
+};
+
+#endif // TRANSACTIONHISTORYSORTFILTRERMODEL_H
