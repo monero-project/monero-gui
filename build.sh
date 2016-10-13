4844bd317111f9c00b3ebbc84207fa6746abb45b
Ilya Kitaev <mbg033@gmail.com>
2016-10-13 15:41:51 +0300

    build: added "debug" build option

--- build.sh
+++ build.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+BUILD_TYPE=$1
+if [ -z $BUILD_TYPE ]; then
+    BUILD_TYPE=Release
+fi
+
+
 source ./utils.sh
 pushd $(pwd)
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MONERO_DIR=monero
 
 if [ ! -d $MONERO_DIR ]; then 
-    $SHELL get_libwallet_api.sh
+    $SHELL get_libwallet_api.sh $BUILD_TYPE
 fi
  
 if [ ! -d build ]; then mkdir build; fi
 
-CONFIG="CONFIG+=release"
+if [ $BUILD_TYPE == "Release" ]; then
+	CONFIG="CONFIG+=release";
+else
+	CONFIG="CONFIG+=debug"
+fi
+
 
 platform=$(get_platform)
 if [ "$platform" == "linux" ]; then
@@ -26,17 +37,4 @@ qmake ../monero-core.pro "$CONFIG"
 make 
 make deploy
 popd
-
-
-
-
-
-
-
-
-
-
-
-
-
 
