05084422a5a0232993abb269b39954cca7708d01
Jaquee <jaquee.monero@gmail.com>
2017-03-30 19:27:58 +0200

    Disable high dpi auto scaling

--- main.cpp
+++ main.cpp
@@ -68,11 +68,11 @@ void messageHandler(QtMsgType type, const QMessageLogContext &context, const QSt
 
 int main(int argc, char *argv[])
 {
-    // Enable high DPI scaling on windows & linux
-#if !defined(Q_OS_ANDROID) && QT_VERSION >= 0x050600
-    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    qDebug() << "High DPI auto scaling - enabled";
-#endif
+//    // Enable high DPI scaling on windows & linux
+//#if !defined(Q_OS_ANDROID) && QT_VERSION >= 0x050600
+//    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+//    qDebug() << "High DPI auto scaling - enabled";
+//#endif
 
     // Log settings
     Monero::Wallet::init(argv[0], "monero-wallet-gui");
