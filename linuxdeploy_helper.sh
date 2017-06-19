5da10d8cfa9366bed39dc1e579e64f7bf2f93e8b
Jonathan Cross <jcross@gmail.com>
2017-06-19 17:31:46 +0200

    Fix font display on Tails

--- linuxdeploy_helper.sh
+++ linuxdeploy_helper.sh
@@ -46,6 +46,12 @@ cat > $TARGET/start-gui.sh <<EOL
 export LD_LIBRARY_PATH=\`pwd\`/libs
 export QT_PLUGIN_PATH=\`pwd\`/plugins
 export QML2_IMPORT_PATH=\`pwd\`/qml
+
+# Fix fonts on Tails
+if [[ -d /usr/share/fonts/truetype/unifont/ ]]; then
+  export QT_QPA_FONTDIR=/usr/share/fonts/truetype/unifont/
+fi
+
 # make it so that it can be called from anywhere and also through soft links
 SCRIPT_DIR="\$(dirname "\$(test -L "\${BASH_SOURCE[0]}" && readlink "\${BASH_SOURCE[0]}" || echo "\${BASH_SOURCE[0]}")")"
 "\$SCRIPT_DIR"/$GUI_EXEC
