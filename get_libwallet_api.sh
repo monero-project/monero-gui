bb127744afc3aac86b9a726af28c495a9d56ad58
dEBRUYNE-1 <dEBRUYNE-1@users.noreply.github.com>
2018-04-02 21:35:28 +0200

    Submodule: use v0.12 tag instead of branch

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout release-v0.12
+git -C $MONERO_DIR checkout v0.12.0.0
 
 # get monero core tag
 get_tag
