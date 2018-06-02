5bb24cfc92c86d8a440ecfa5118a6fa3d02c7d40
dEBRUYNE-1 <dEBRUYNE-1@users.noreply.github.com>
2018-06-02 19:19:47 +0200

    Update submodule for v0.12.2.0

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout v0.12.1.0
+git -C $MONERO_DIR checkout v0.12.2.0
 
 # get monero core tag
 get_tag
