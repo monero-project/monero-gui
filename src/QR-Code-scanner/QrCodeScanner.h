352da92229fc30b6494c46e7a3e7d9ed80521bb5
xiphon <xiphon@protonmail.com>
2020-07-16 17:13:42 +0000

    QrCodeScanner: use parse_uri_to_object to parse scanned codes

--- src/QR-Code-scanner/QrCodeScanner.h
+++ src/QR-Code-scanner/QrCodeScanner.h
@@ -51,13 +51,12 @@ public:
     void setEnabled(bool enabled);
 
 public Q_SLOTS:
-    void processCode(int type, const QString &data);
     void processFrame(QVideoFrame);
 
 Q_SIGNALS:
     void enabledChanged();
 
-    void decoded(const QString &address, const QString &payment_id, const QString &amount, const QString &tx_description, const QString &recipient_name, const QVariantMap &extra_parameters);
+    void decoded(int type, const QString &data);
     void decode(int type, const QString &data);
     void notifyError(const QString &error, bool warning = false);
 
