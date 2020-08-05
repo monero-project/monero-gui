9ce88c6aeb815d15ba6bf05f2f5822407294d0bf
selsta <selsta@sent.at>
2020-08-05 14:24:27 +0200

    build: prepare v0.16.0.3

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout v0.16.0.1
+git -C $MONERO_DIR checkout v0.16.0.3
 
 # get monero core tag
 pushd $MONERO_DIR
