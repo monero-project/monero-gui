e36b166edde49f5943e745137c1796beccdb08bb
tobtoht <thotbot@protonmail.com>
2021-04-24 05:51:19 +0200

    Wallet: import / export output function

--- src/libwalletqt/Wallet.h
+++ src/libwalletqt/Wallet.h
@@ -208,6 +208,10 @@ public:
     Q_INVOKABLE bool exportKeyImages(const QString& path);
     Q_INVOKABLE bool importKeyImages(const QString& path);
 
+    //! export/import outputs
+    Q_INVOKABLE bool exportOutputs(const QString& path, bool all = false);
+    Q_INVOKABLE bool importOutputs(const QString& path);
+
     //! refreshes the wallet
     Q_INVOKABLE bool refresh(bool historyAndSubaddresses = true);
 
