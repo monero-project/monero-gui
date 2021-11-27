f8d791280acd149aaea7ed0d060cbed78fadbdf7
castorp <masimov@me.com>
2021-11-27 09:33:56 +0000

    Ukrainian
    Currently translated at 80.5% (583 of 724 strings)
    Translate-URL: https://translate.getmonero.org/projects/monero/gui-wallet/uk/
    Author: castorp <masimov@me.com>
    
    Co-authored-by: castorp <masimov@me.com>

--- translations/monero-core_uk.ts
+++ translations/monero-core_uk.ts
@@ -259,7 +259,7 @@
     <message>
         <location filename="../components/DevicePassphraseDialog.qml" line="44"/>
         <source>Hardware wallet</source>
-        <translation>Апаратний гаманець</translation>
+        <translation>Пристрій-гаманець</translation>
     </message>
     <message>
         <location filename="../components/DevicePassphraseDialog.qml" line="45"/>
