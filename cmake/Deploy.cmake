4743e443dc801bafa86ca9482329014b0eb9b03c
selsta <selsta@sent.at>
2022-11-01 00:11:42 +0100

    deploy: update libicu to 72

--- cmake/Deploy.cmake
+++ cmake/Deploy.cmake
@@ -74,19 +74,19 @@ if(APPLE OR (WIN32 AND NOT STATIC))
         )
         if(CMAKE_BUILD_TYPE STREQUAL "Debug")
             list(APPEND WIN_DEPLOY_DLLS
-                libicudtd71.dll
-                libicuind71.dll
-                libicuiod71.dll
-                libicutud71.dll
-                libicuucd71.dll
+                libicudtd72.dll
+                libicuind72.dll
+                libicuiod72.dll
+                libicutud72.dll
+                libicuucd72.dll
             )
         else() # assume release
             list(APPEND WIN_DEPLOY_DLLS
-                libicudt71.dll
-                libicuin71.dll
-                libicuio71.dll
-                libicutu71.dll
-                libicuuc71.dll
+                libicudt72.dll
+                libicuin72.dll
+                libicuio72.dll
+                libicutu72.dll
+                libicuuc72.dll
             )
         endif()
         list(TRANSFORM WIN_DEPLOY_DLLS PREPEND "$ENV{MSYSTEM_PREFIX}/bin/")
